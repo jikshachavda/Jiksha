7d904b6f243f153bf80b289d9289376a4967a6b9
jikshachavda <jikshachavda@gmail.com>
2023-08-27 15:31:37 +0530

    my portfolio

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import About from './About';
+import './App.css';
+import Home from './Home';
+import Projects from './Projects';
+import Skill from './Skill';
+
+function App() {
+  return (
+    <>
+
+<BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/skill" element={<Skill/>}></Route>
+          <Route path="/project" element={<Projects/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
